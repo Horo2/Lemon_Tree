f15984a120422acbe321e76f027b174c41e51527
WhoLoser <dbing@uci.edu>
2023-11-02 02:45:07 -0700

    GameLoop1.0

---
+++ Lemon_Tree/Assets/Scripts/Menu/UIManager.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject mainMenu;
+    public string gameSceneName;
+    public string mainMenuSceneName;
+
+    // Remove gameState reference as we will use singleton
+
+    // private bool isGamePaused; removed as we will use GameStateManager
+
+    private void Start()
+    {
+        // Time.timeScale set in GameStateManager
+        pauseMenu.SetActive(false);
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == mainMenuSceneName)
+        {
+            GameStateManager.Instance.ChangeGameState(GameStateManager.GameState.MainMenu);
+            mainMenu.SetActive(true);
+        }
+        else
+        {
+            mainMenu.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        GameStateManager.Instance.ChangeGameState(GameStateManager.GameState.InGame);
+        SceneManager.LoadScene(gameSceneName);
+
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+
+    public void PauseGame()
+    {
+        // Use GameStateManager instead of isGamePaused
+        if (GameStateManager.Instance.currentGameState == GameStateManager.GameState.InGame)
+        {
+            GameStateManager.Instance.ChangeGameState(GameStateManager.GameState.Paused);
+            pauseMenu.SetActive(true);
+        }
+    }
+
+    public void ReturnToMainMenu()
+    {
+        GameStateManager.Instance.ChangeGameState(GameStateManager.GameState.MainMenu);
+        SceneManager.LoadScene(mainMenuSceneName);
+        pauseMenu.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        // Use GameStateManager instead of isGamePaused
+        if (GameStateManager.Instance.currentGameState == GameStateManager.GameState.Paused)
+        {
+            GameStateManager.Instance.ChangeGameState(GameStateManager.GameState.InGame);
+            pauseMenu.SetActive(false);
+
+        }
+    }
+
+    public void LoadCurrentScene()
+    {
+        GameStateManager.Instance.ChangeGameState(GameStateManager.GameState.InGame);
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
+    }
+}
