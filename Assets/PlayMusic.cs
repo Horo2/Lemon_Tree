5144db0ad0a61376271a1a8f30679d74f8138572
little-fa-fa <maria.zhangj@gmail.com>
2023-11-08 00:40:00 -0800

    Level1 Puzzle Basic Added

---
+++ Assets/PlayMusic.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PlayMusic : MonoBehaviour
+{
+    public List<GameObject> socketContainers; // List of the socket containers (each child object)
+    private List<bool> socketFilledStates; // List to keep track of whether sockets are filled
+    public List<AudioClip> musicTracks; // List of music tracks to play based on the state
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        // Initialize the socketFilledStates list with all sockets initially unfilled
+        socketFilledStates = new List<bool>(new bool[socketContainers.Count]);
+    }
+
+    private void Update()
+    {
+        // Check for changes in socket states and update the audio track accordingly
+        for (int i = 0; i < socketContainers.Count; i++)
+        {
+            XRSocketInteractor socketInteractable = socketContainers[i].GetComponentInChildren<XRSocketInteractor>();
+
+            if (socketInteractable != null)
+            {
+                bool socketFilled = socketInteractable.hasSelection; // Check if the socket is filled
+
+                if (socketFilled != socketFilledStates[i])
+                {
+                    socketFilledStates[i] = socketFilled;
+                    PlayMusicBasedOnState();
+                }
+            }
+        }
+    }
+
+    private void PlayMusicBasedOnState()
+    {
+        Debug.Log("Switching Music");
+        // Generate a key based on the socketFilledStates list
+        string stateKey = string.Join("-", socketFilledStates.ConvertAll(x => x ? "1" : "0").ToArray());
+        Debug.Log("statekey: " + stateKey);
+        // Search for a matching state in the musicTracks list
+        int index = musicTracks.FindIndex(track => track.name == stateKey);
+        Debug.Log("index: " + index);
+        // If a matching state is found, play the corresponding music
+        if (index >= 0)
+        {
+            audioSource.clip = musicTracks[index];
+            audioSource.Play();
+        }
+    }
+}
