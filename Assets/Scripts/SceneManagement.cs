67627b9b11bb15d08f497c057cf58565123debca
iamamAYem <xjing2@uci.edu>
2023-11-14 01:29:24 -0800

    JZ set up

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+
+    public string name;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SwitchScene();
+        }
+    }
+
+    void SwitchScene()
+    {
+        //家人们，准备传送咯
+        SceneManager.LoadScene(name);
+    }
+}
