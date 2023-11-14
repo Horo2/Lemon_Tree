5cca64e574c191905e357b58faa18e1498a730fd
ShubingZ <shubingz@uci.edu>
2023-11-14 00:05:21 -0800

    cave made

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+
+    private void Awake()
+    {
+        // Singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayMusic(AudioClip musicClip, float volume = 1.0f)
+    {
+        musicSource.clip = musicClip;
+        musicSource.volume = volume;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip sfxClip, float volume = 1.0f)
+    {
+        sfxSource.PlayOneShot(sfxClip, volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
+}
